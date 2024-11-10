f8a4805cf9dc8a26b37d577f443ef2cdc80c3728
codegreen <codegreen19s@gmail.com>
2024-11-10 07:45:01 +0600

    first commit

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MarketingPage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat rem
+      voluptatum autem ea quia sapiente perspiciatis ducimus labore quaerat
+      delectus, molestias quidem voluptas ex tempora accusantium necessitatibus
+      numquam totam voluptates.
+    </div>
+  );
+};
+
+export default MarketingPage;
